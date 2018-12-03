5567cd1242ac5d1142d545e06920c9c860dce776
andjim <andjim2598@gmail.com>
2018-12-03 12:53:17 -0400

    finished desing

--- index.js
+++ index.js
@@ -1409,21 +1409,26 @@ let yo = require("yo-yo");
 let main = document.getElementById('main');
 let header = yo`
 <div class="header ">
-		<div class="container">
+		<div class="cont">
 			<div class="row">
-				<div class="textbox col s8 push-s4">
-					<div class="HText f col s12">
-						<p>I'm</p>
-					</div>
-					<div class="HText s col s12">
-						<h2 class="" >Anderson Jimenez</h2>
-					</div>
-					<div class="HText t col s12">
-						<p>Student of System ingeniering and web devoloper.</p>
-					</div>
+				<div class=" side1 col s1">
+                    <ul>
+					    <li><p><a href="#skills">Skills</a></p></li>
+					    <li><p><a href="#contact" >Contact</a></p></li>
+                    </ul>
 				</div>
-				<div class="col s4 pull-s8">
-					<img class='circle responsive-img'src="./assets/yo2.png"/>
+				<div class="side2 col s11">
+					<div class=" photo col s12">
+						<img class='circle responsive-img'src="./assets/yo2.png"/>
+					</div>
+					<div class="textbox col s12">
+						<div class="HText s col s12">
+							<h1 class="" >Anderson Jimenez</h1>
+						</div>
+						<div class="HText t col s12">
+							<h4>Software Engeniering student and Web Developer</h4>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -1435,54 +1440,34 @@ let section = yo`
 <div class=" section">
 	<div class="container">
 			<div class=" row">
-				<div class="col s12"><p>Skills and knowledge areas</p></div>
-				<div class="col s6">
-					<div class="col s12">
+				<div id="skills" class="col s12"><h2 >Skills</h2></div>
+				<div class="skillBox col s12">
+					<div class=" skill col s12">
 						<div class="col s4">
 							<img class="responsive-img circle" src="./assets/icon/wd.png"/>
 						</div>
 						<div  class="col s6">
 							<span>Web Design</span>
-							<meter class="col s12" value="0.2">60%</meter>
+							<meter class="col s12" value="0.2">20%</meter>
 						</div>
 					</div>
-					<div class="col s12">
+					<div class="skill col s12">
 						<div class="col s4">
 							<img class="responsive-img circle" src="./assets/icon/js.png"/>
 						</div>
 						<div  class="col s6">
 							<span>Javascript</span>
-							<meter  class="col s12" value="0.4">60%</meter>
+							<meter  class="col s12" value="0.4">40%</meter>
 						</div>
 					</div>
-					<div class="col s12">
+					<div class="skill col s12">
 						<div class="col s4">
 							<img class="responsive-img circle" src="./assets/icon/py.png"/>
 						</div>
 						<div  class="col s6">
 							<span>Python</span>
-							<meter class="col s12" value="0.3">60%</meter>
+							<meter class="col s12" value="0.3">30%</meter>
 						</div>
-					</div>
-				</div>
-				<div class="col s6">
-					<div class="col s12">
-						<figure>
-							<img src=""/>FOTO
-						</figure>
-							<p>I'm</p>
-						</div>
-					<div class="col s12">
-						<figure>
-							<img src=""/>FOTO2
-						</figure>
-						<p>Anderson Jimenez Santana,</p>
-					</div>
-					<div class="col s12">
-						<figure>
-							<img src=""/>FOTO3
-						</figure>
-						<p>Student of System ingeniering and web devoloper.</p>
 					</div>
 				</div>
 			</div>
@@ -1490,4 +1475,56 @@ let section = yo`
 </div>
 `;
 main.appendChild(section);
+
+let contact = yo`
+    <div class="contact">
+        <div class="row">
+            <div class="textAbout col s12 l7">
+                <div class="col s12">
+                    <h2 id="contact">About me</h2>
+                </div>
+                <div class="col s12">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Fusce sollicitudin, nisi a pulvinar convallis, sapien orci tincidunt 
+                        odio, vel bibendum dolor justo in elit. Aenean consequat nibh  lacus, 
+                        vitae  mollis  urna  ornare  sed. Duis  porttitor feugiat felis,  a 
+                        ultrices mauris porttitor a. Pellentesque congue tincidunt enim, in placerat 
+                        mi volutpat non. Integer nec pellentesque metus. Nam venenatis, felis eu luctus auctor, 
+                        velit libero tempus odio, eu blandit magna mi vitae enim. Maecenas sed dui nec leo commodo 
+                        facilisis nec scelerisque arcu. Nam urna ipsum, dictum in neque et, interdum 
+                        suscipit magna. Integer suscipit risus ut ornare feugiat.
+                    </p>
+                </div>
+                <div class="col s12">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Fusce sollicitudin, nisi a pulvinar convallis, sapien orci tincidunt 
+                        odio, vel bibendum dolor justo in elit. Aenean consequat nibh  lacus, 
+                        vitae  mollis  urna  ornare  sed. Duis  porttitor feugiat felis,  a 
+                        ultrices mauris porttitor a. Pellentesque congue tincidunt enim, in placerat 
+                        mi volutpat non. Integer nec pellentesque metus. Nam venenatis, felis eu luctus auctor, 
+                        velit libero tempus odio, eu blandit magna mi vitae enim. Maecenas sed dui nec leo commodo 
+                        facilisis nec scelerisque arcu. Nam urna ipsum, dictum in neque et, interdum 
+                        suscipit magna. Integer suscipit risus ut ornare feugiat.
+                    </p>
+                </div>
+            </div>
+            <div class="contactInfo col s12 l5">
+                <div class="col s12">
+                    <h2>Contact</h2>
+                </div>
+                <div class="contactPic col s12">
+                    <img class='circle responsive-img'src="./assets/yo2.png"/>
+                </div>
+                <div class="fb col s12">
+                    <div><i class="fab fa-facebook"></i><span><a href="https://www.facebook.com/anderson.jimenez.129794">Anderson Jimenez</a></span></div>
+                </div>
+                <div class="email col s12">
+                    <div><i class="fas fa-address-book"></i><span><a>Andjim2598@gmail.com</a></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+main.appendChild(contact);
 },{"yo-yo":10}]},{},[12]);
